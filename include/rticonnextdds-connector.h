8952bab7a607133169235300d90229a349df1be7
Kyoungho An <kyoungho.an@gmail.com>
2018-10-19 12:15:07 -0700

    updated license header

--- include/rticonnextdds-connector.h
+++ include/rticonnextdds-connector.h
@@ -1,10 +1,12 @@
-/******************************************************************************
-* (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.       *
-* No duplications, whole or partial, manual or electronic, may be made        *
-* without express written permission.  Any such copies, or revisions thereof, *
-* must display this notice unaltered.                                         *
-* This code contains trade secrets of Real-Time Innovations, Inc.             *
-******************************************************************************/
+/*****************************************************************************
+*   (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.    *
+*                                                                            *
+* No duplications, whole or partial, manual or electronic, may be made       *
+* without express written permission.  Any such copies, or revisions thereof,*
+* must display this notice unaltered.                                        *
+* This code contains trade secrets of Real-Time Innovations, Inc.            *
+*                                                                            *
+*****************************************************************************/
 #include "lua_binding/lua_binding_ddsConnector.h"
 
 void RTIDDSConnector_write(void *connector, char *name, char *param);
