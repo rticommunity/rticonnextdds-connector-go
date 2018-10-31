4d02bbd8e6ec02d4bf2375e77cf7b94822d0d216
Kyoungho An <kyoungho@rti.com>
2018-10-31 11:17:57 -0700

    added test configs for darwin

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$PWD/rticonnextdds-connector/lib/x64Linux2.6gcc4.4.5:$LD_LIBRARY_PATH
-go test -v -race -coverprofile=coverage.txt -covermode=atomic 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	export LD_LIBRARY_PATH=$PWD/rticonnextdds-connector/lib/x64Linux2.6gcc4.4.5:$LD_LIBRARY_PATH
+	go test -v -race -coverprofile=coverage.txt -covermode=atomic 
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	export DYLD_LIBRARY_PATH=rticonnextdds-connector/lib/x64Darwin16clang8.0:$DYLD_LIBRARY_PATH
+	go test -v -race -coverprofile=coverage.txt -covermode=atomic 
+fi
