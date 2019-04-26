de2dcc7449a186e4048011b750bddb659523bea6
Kyoungho An <kyoungho@rti.com>
2019-04-26 12:22:29 -0700

    added examples for array

---
+++ examples/array_writer/array_writer.go
@@ -0,0 +1,62 @@
+/*****************************************************************************
+*   (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.    *
+*                                                                            *
+* No duplications, whole or partial, manual or electronic, may be made       *
+* without express written permission.  Any such copies, or revisions thereof,*
+* must display this notice unaltered.                                        *
+* This code contains trade secrets of Real-Time Innovations, Inc.            *
+*                                                                            *
+*****************************************************************************/
+
+package main
+
+import (
+	"github.com/rticommunity/rticonnextdds-connector-go"
+	"path"
+	"runtime"
+	"time"
+	"log"
+)
+
+type Shape struct {
+        Color     string `json:"color"`
+        X         [100]int    `json:"x"`
+        Y         [100]int    `json:"y"`
+        Shapesize int    `json:"shapesize"`
+}
+
+func main() {
+	// Find the file path to the XML configuration
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Panic("runtime.Caller error")
+	}
+	filepath := path.Join(path.Dir(filename), "../ShapeArrayExample.xml")
+
+	// Create a connector defined in the XML configuration
+	connector, err := rti.NewConnector("MyParticipantLibrary::Zero", filepath)
+	if err != nil {
+		log.Panic(err)
+	}
+	// Delete the connector when this main function returns
+	defer connector.Delete()
+
+	// Get an output from the connector
+	output, err := connector.GetOutput("MyPublisher::MySquareWriter")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Set values to the instance and write the instance
+	for i := 0; i < 500; i++ {
+		var shape Shape
+		shape.X[i%100] = i
+		shape.Y[i%100] = i*2
+		shape.Shapesize = 30
+		shape.Color = "BLUE"
+		output.Instance.Set(&shape)
+		output.Write()
+		log.Println("Writing...")
+		time.Sleep(time.Second * 1)
+	}
+}
