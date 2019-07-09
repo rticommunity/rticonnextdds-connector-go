6bf59b40a2e40de58bdc44b7cccaed5fc3718169
Kyoungho An <kyoungho.an@gmail.com>
2019-07-08 20:12:56 -0700

    move golangci to travis

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 go get -v github.com/stretchr/testify
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.17.1
-
-golangci-lint run
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	export LD_LIBRARY_PATH=$PWD/rticonnextdds-connector/lib/x64Linux2.6gcc4.4.5:$LD_LIBRARY_PATH
