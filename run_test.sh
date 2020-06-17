46c8db45074f05aa32c6d38fd53fa471652f3706
Kyoungho An <kyoungho@rti.com>
2020-06-17 12:05:07 -0700

    updated the test script

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 
 go get -v github.com/stretchr/testify
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ "$OSTYPE" == "linux"* ]] ; then
 	export LD_LIBRARY_PATH=$PWD/rticonnextdds-connector/lib/x64Linux2.6gcc4.4.5:$LD_LIBRARY_PATH
 	go test -v -race -coverprofile=coverage.txt -covermode=atomic 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
