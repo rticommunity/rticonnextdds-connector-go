0199f371141b7db33cd38b009dcfc1311461ece8
Kyoungho An <kyoungho@rti.com>
2022-03-02 10:18:13 -0600

    Resolve warnings from ld

--- build_dynamic.go
+++ build_dynamic.go
@@ -3,7 +3,7 @@
 package rti
 
 // #cgo linux,amd64 CFLAGS: -I${SRCDIR}/include -I${SRCDIR}/rticonnextdds-connector/include -DRTI_UNIX -DRTI_LINUX -DRTI_64BIT
-// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/rticonnextdds-connector/lib/linux-x64 -lrtiddsconnector -ldl -lnsl -lm -lpthread -lrt
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/rticonnextdds-connector/lib/linux-x64 -lrtiddsconnector -ldl -lnsl -lm -lpthread -lrt -Wl,-rpath,${SRCDIR}/rticonnextdds-connector/lib/linux-x64
 // #cgo darwin CFLAGS: -I${SRCDIR}/include -I${SRCDIR}/rticonnextdds-connector/include -DRTI_UNIX -DRTI_DARWIN -DRTI_DARWIN10 -DRTI_64BIT -m64
 // #cgo darwin LDFLAGS: -L${SRCDIR}/rticonnextdds-connector/lib/osx-x64 -lrtiddsconnector -ldl -lm -lpthread
 import "C"
